ea7b39aaf615b2a727942314f75f0320acdb20bc
Kashif saeed <kashif.y.saeed@gmail.com>
2018-04-14 18:43:04 +0500

    user alert message display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_index_path,notice: 'User created successfully!'
+      redirect_to admin_index_path, notice: 'User created successfully!'
     else
       render :new
     end
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
     @user.skip_password_validation = true
     
     if @user.update(user_params)
-      redirect_to admin_index_path
+      redirect_to admin_index_path, notice: "User updated successfully!"
     else
       render :edit
     end
